8bc6adfb44862825be0eeeb34de9d2c4c9811ca1
syedfaizalalatas <syedfaizalalatas@gmail.com>
2018-11-08 11:14:43 +0800

    menu sisi boleh bergerak
    menu sisi tidak boleh bergerak menjadi masalah jika ia tersorok di bawah.
    menghilangkan class "menu_fixed"

--- repositori/views/layouts/lay_sidebarfull.php
+++ repositori/views/layouts/lay_sidebarfull.php
@@ -1,4 +1,4 @@
-        <div class="col-md-3 left_col menu_fixed">
+        <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
               <a href="../sysmgmt/dashboard.php" class="site_title" style="display: none;"><i class="fa fa-globe"></i> <span>SRP 1.0</span></a>
